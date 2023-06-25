459409ae0ed2c337ae3a18fc1b5bdc13ab46e223
Adrian McPhee <adrianmcphee@amc.home>
2023-06-25 12:57:29 +0200

    Setup example of data flow from json into homepage

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import os
+import json
 
 app = Flask(__name__)
 
@@ -6,13 +8,26 @@ app = Flask(__name__)
 def screen_list():
     return render_template('screen-list.html')
 
+#homepage views
+
 @app.route('/')
 def home():
-    return 'Homepage with list of challenges'
+    challenge_listings_file = os.path.abspath("data-sets/challenge_listings.json")
+    with open(challenge_listings_file) as json_file:
+        raw_challenge_listings = json.load(json_file)
+    challenge_listings = raw_challenge_listings["data"]["challengelisting"]
+    return render_template("home.html", challenge_listings=challenge_listings)
 
 @app.route('/products')
 def home_products():
     return 'Homepage with list of products'
+
+#product pages
+
+@app.route('/product/summary')
+def product_summary():
+    return 'Product page with Summary'
+
 
 @app.route('/users/sign-up')
 def user_sign_up():
