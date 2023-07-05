edc153a335cb0299489e1dea957c06a8e8952f81
Iryna Vynnychenko <i.vynnychenko@gmail.com>
2023-07-05 18:42:14 +0300

    py + json + js connection for product table

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ import json
 
 app = Flask(__name__)
 
+
+
 @app.route('/screen-list')
 def screen_list():
     return render_template('screen_list.html')
@@ -31,10 +33,26 @@ def home_products():
     return render_template("home_products.html", products_listings=products_listings)
 
 #product pages
+def generate_summary_structure(data):
+    html = ""
+    for item in data:
+        html += "<div class='nested-item pl-3'>"
+        html += f'<div class="nested-item__label"><span class="nested-item__label-icon">+</span><a href="{item["link"]}">{item["name"]}</a></div>'
+        if item["children"]:
+            html += f'<div class="nested-item__child hidden">'
+            html += generate_summary_structure(item["children"])
+            html += "</div>"
+        html += "</div>"
+    return html
 
 @app.route('/product/summary')
 def product_summary():
-    return render_template("product_summary.html", current_page="summary_page")
+    product_summary_listings_file = os.path.abspath("data-sets/product_summary_listings.json")
+    with open(product_summary_listings_file) as json_file:
+        raw_product_summary_listings = json.load(json_file)
+    product_summary_listings = raw_product_summary_listings["data"]["summary"]
+    html_structure = generate_summary_structure(product_summary_listings)
+    return render_template("product_summary.html", html_structure=html_structure, current_page="summary_page")
 
 @app.route('/product/initiatives')
 def product_initiatives():
