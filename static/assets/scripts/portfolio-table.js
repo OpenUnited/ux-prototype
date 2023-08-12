680e7943f8c7785bef9c0a255a81635a2c297e33
Iryna Vynnychenko <i.vynnychenko@gmail.com>
2023-08-12 20:46:41 +0300

    popup for table item

--- static/assets/scripts/portfolio-table.js
+++ static/assets/scripts/portfolio-table.js
@@ -39,3 +39,48 @@ if (button) {
 
   });
 }
+
+function cutCopyAndRemove(targetElement, sourceElement) {
+
+  if (sourceElement && targetElement) {
+    const clonedNode = sourceElement.cloneNode(true);
+    
+    targetElement.appendChild(clonedNode); // Вставляем клонированный элемент
+    sourceElement.parentNode.removeChild(sourceElement); // Удаляем исходный элемент
+    return clonedNode;
+  }
+
+}
+
+const portfolioTable = document.querySelector('#portfolio-table');
+const rows = document.querySelectorAll(".table-row");
+
+rows.forEach((row) => {
+  const tableLink = row.querySelector('[data-table-link]');
+  
+  let clonedPopup;
+
+  tableLink?.addEventListener("mouseover", (event) => {
+    const tablePopup = row.querySelector('[data-table-popup]');
+    if(!tablePopup) return
+    tablePopup.classList.remove('hidden');
+    clonedPopup = cutCopyAndRemove(portfolioTable, tablePopup);
+
+    clonedPopup.style.position = "absolute";
+    clonedPopup.style.top = `${tableLink.offsetTop - clonedPopup.clientHeight}px`; // Adjust the positioning as needed
+    clonedPopup.style.left = `${tableLink.offsetLeft}px`;
+
+    clonedPopup.addEventListener("mouseleave", (event) => {
+      clonedPopup.classList.add('hidden');
+      clonedPopup = cutCopyAndRemove(tableLink.parentElement, clonedPopup);
+    })
+  });
+
+  tableLink?.addEventListener("mouseleave", (event) => {
+    if (event.relatedTarget === clonedPopup) return;
+
+    clonedPopup.classList.add('hidden');
+    clonedPopup = cutCopyAndRemove(tableLink.parentElement, clonedPopup);
+
+  });
+});
